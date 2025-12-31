e26f844cf00fa0ca1b923c2f56b95924c745e4f5
cyberworrior17 <amraansaleh009@gmail.com>
2026-01-01 01:53:08 +0300

    Update logo.py

--- logo.py
+++ logo.py
@@ -1,18 +1,14 @@
 def logo():
-    GREEN = "\033[1;92m"
-    RESET = "\033[0m"
-
-    print(GREEN)
+    print("\033[1;92m")
     print(" ███████╗ ██╗   ██╗ ██╗ ██████╗ ██╗   ██╗ ███████╗")
     print(" ██╔════╝ ██║   ██║ ██║ ██╔══██╗██║   ██║ ██╔════╝")
     print(" █████╗   ██║   ██║ ██║ ██████╔╝██║   ██║ ███████╗")
     print(" ██╔══╝   ╚██╗ ██╔╝ ██║ ██╔══██╗██║   ██║ ╚════██║")
     print(" ██║       ╚████╔╝  ██║ ██║  ██║╚██████╔╝ ███████║")
     print(" ╚═╝        ╚═══╝   ╚═╝ ╚═╝  ╚═╝ ╚═════╝  ╚══════╝")
-    print(RESET)
-
-    print("        F  V I R U S")
-    print("  PEGASUSA TZ TOOL\n")
-    print("Coded by Mr.Virus (Tanzania)")
+    print("\033[0m")
+    print("These tool coded by Mr.Virus (Tanzania)")
     print("Use for educational purposes only")
     print("IG: @uknown_virus404x\n")
+
+
